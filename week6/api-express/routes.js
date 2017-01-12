1802d3697c32eb0e8b77ae69cf72caa9b364b13f
kevin.lefevre <kevin.lefevre@gmail.com>
2017-01-12 14:00:49 -0500

    more commits

--- week6/api-express/routes.js
+++ week6/api-express/routes.js
@@ -1,12 +1,18 @@
+//require the controller files
 
 var API = require('./controllers/api');
 
+//we register routes with methods like 'app.get', 'app.post', etc. How do we get 'app' into this file from server.js?
+//normally we export objects, but we can export functions too.
 
+//this function will be called in 'server.js' and it will pass in the 'app object.
 
 module.exports = (app) => {
     
-    app.post('/api/booze', API.create);
+    //creating a new album api, passing it a function
+    app.post('/api/album', api.album);
     
 };
 
 
+//JSON needs " " for strings and no trailing comas
